f31dfb0e6f239dd7ef9314c9420e8d7202c04f85
Msea <15201331135@163.com>
2019-07-16 17:27:08 +0800

    Update http.js

--- src/core/api/http.js
+++ src/core/api/http.js
@@ -37,4 +37,13 @@ let ajaxConfig={
 
 var instance = axios.create(ajaxConfig)
 
+instance.interceptors.request.use(function (config) {
+    // 在发送请求之前做些什么
+    console.log(config);
+    return config;
+  }, function (error) {
+    // 对请求错误做些什么
+    return Promise.reject(error);
+  });
+
 export default instance;
